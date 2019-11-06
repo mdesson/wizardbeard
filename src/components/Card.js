e19d0b3e5570068e4923f03341935264175ac6b8
Mike <michael.desson@gmail.com>
2019-11-06 18:57:12 -0500

    Set up react select

--- src/components/Card.js
+++ src/components/Card.js
@@ -41,19 +41,4 @@ const prettyLevel = level => {
   else return level + 'th level'
 }
 
-var example = {
-  name: 'Acid Arrow',
-  school: 'Evocation',
-  duration: 'instantaneous',
-  ritual: 'no',
-  material: 'Powdered rhubarb leaf and an adder’s stomach.',
-  components: ['V', 'S', 'M'],
-  range: '90 feet',
-  concentration: 'no',
-  casting_time: '1 action',
-  level: 2,
-  description:
-    'A shimmering green arrow streaks toward a target within range and bursts in a spray of acid. Make a ranged spell attack against the target. On a hit, the target takes 4d4 acid damage immediately and 2d4 acid damage at the end of its next turn. On a miss, the arrow splashes the target with acid for half as much of the initial damage and no damage at the end of its next turn.'
-}
-
 export default Card
--- src/views/SpellList.js
+++ src/views/SpellList.js
@@ -1,27 +1,84 @@
 import React from 'react'
+import Select from 'react-select'
 import Card from '../components/Card.js'
 import './SpellList.css'
 import spells from '../mockdata.js'
 
-const SpellList = () => {
-  var spell = {
-    name: 'Acid Arrow',
-    school: 'Evocation',
-    duration: 'Instantaneous',
-    ritual: 'no',
-    material: 'Powdered rhubarb leaf and an adder’s stomach.',
-    components: ['V', 'S', 'M'],
-    range: '90 feet',
-    concentration: 'no',
-    casting_time: '1 action',
-    level: 2,
-    description:
-      'A shimmering green arrow streaks toward a target within range and bursts in a spray of acid. Make a ranged spell attack against the target. On a hit, the target takes 4d4 acid damage immediately and 2d4 acid damage at the end of its next turn. On a miss, the arrow splashes the target with acid for half as much of the initial damage and no damage at the end of its next turn.'
+const options = [
+  {
+    label: 'School',
+    options: [
+      { value: 'Abjuration', label: '🛡️ Abjuration' },
+      { value: 'Conjuration', label: '🦄 Conjuration' },
+      { value: 'Divination', label: '🔮 Divination' },
+      { value: 'Enchantment', label: '✨ Enchantment' },
+      { value: 'Evocation', label: '💥 Evocation' },
+      { value: 'Illusion', label: '🌫️ Illusion' },
+      { value: 'Necromancy', label: '💀 Necromancy' },
+      { value: 'Transmutation', label: '🎭 Transmutation' }
+    ]
+  },
+  {
+    label: 'Level',
+    options: [
+      { value: -1, label: 'Cantrip' },
+      { value: 1, label: 'Level 1' },
+      { value: 2, label: 'Level 2' },
+      { value: 3, label: 'Level 3' },
+      { value: 4, label: 'Level 4' },
+      { value: 5, label: 'Level 5' },
+      { value: 6, label: 'Level 6' },
+      { value: 7, label: 'Level 7' },
+      { value: 8, label: 'Level 8' },
+      { value: 9, label: 'Level 9' }
+    ]
+  },
+  {
+    label: 'Ritual Spell',
+    options: [
+      { value: 'yes', label: 'Is Ritual' },
+      { value: 'no', label: 'Not Ritual' }
+    ]
+  },
+  {
+    label: 'Concentration Spell',
+    options: [
+      { value: 'yes', label: 'Is Concentration' },
+      { value: 'no', label: 'Not Concentration' }
+    ]
   }
+]
 
+const styles = {
+  option: base => ({
+    ...base,
+    color: 'black'
+  })
+}
+
+const formatGroupLabel = data => (
+  <div>
+    <span>{data.label}</span>
+  </div>
+)
+
+const SpellList = () => {
   return (
     <div>
-      <h1>One day I will be a searchbar!</h1>
+      <div className="Description-text">
+        <div>
+          Welcome to <b>Wizard Beard</b>! The most convenient place on this
+          plane to manage your spells!
+        </div>
+      </div>
+      <div style={{ marginLeft: 50, marginRight: 50, align: 'center' }}>
+        <Select
+          isMulti
+          styles={styles}
+          formatGroupLabel={formatGroupLabel}
+          options={options}
+        />
+      </div>
       <div className="Spell-list">
         {spells.map(s => (
           <Card spell={s} />
