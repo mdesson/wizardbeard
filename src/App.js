cc202a169702df53c823b05edbc8226a46788c99
mdesson <michael.desson@gmail.com>
2019-11-07 23:03:15 -0500

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import SpellList from './views/SpellList.js'
+import Character from './views/Character'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <span className="Wizard-header" role="img" aria-label="wizard">
+    <div className='App'>
+      <header className='App-header'>
+        <span className='Wizard-header' role='img' aria-label='wizard'>
           🧙
         </span>
-        <span className="Title-header">Wizard Beard</span>
+        <span className='Title-header'>Wizard Beard</span>
       </header>
-      <div className="Underline"></div>
-      <SpellList />
+
+      <Router>
+        {/* Router Links */}
+        <div className='App-menu'>
+          <Link className='App-menu-item' to='/'>
+            Spell List
+          </Link>
+          <Link className='App-menu-item' to='/mybook'>
+            My Spell Book
+          </Link>
+        </div>
+
+        <div className='Underline'></div>
+
+        {/* Content and Router Swtich */}
+        <Switch>
+          <Route path='/mybook'>
+            <Character />
+          </Route>
+          <Route path='/'>
+            <SpellList />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
---
+++ src/views/Character.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Character = () => {
+  return <div>What a story, Mark!</div>
+}
+
+export default Character
