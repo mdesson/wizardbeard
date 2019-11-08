051f5d064c777e3b205968dba09896511a7e54e6
mdesson <michael.desson@gmail.com>
2019-11-07 23:22:49 -0500

    Added firebase

--- src/views/SpellList.js
+++ src/views/SpellList.js
@@ -4,7 +4,6 @@ import Card from '../components/Card.js'
 import SpellFilter from '../components/SpellFilter.js'
 import './SpellList.css'
 
-// TODO: Add filtering and card support for class
 const SpellList = () => {
   const [spells, setSpells] = useState([])
   const [filteredSpells, setFilteredSpells] = useState(spells)
@@ -28,22 +27,18 @@ const SpellList = () => {
 
   return (
     <div>
-      <div className="Description-text">
+      <div className='Description-text'>
         <div>
           Welcome to <b>Wizard Beard</b>! The most convenient place on this plane to manage your spells!
         </div>
       </div>
       {isLoading ? (
-        <div className="Loading-text">Loading content</div>
+        <div className='Loading-text'>Loading content</div>
       ) : (
         <div>
           <SpellFilter onChange={setFilteredSpells} spells={spells} />
-          <div className="Spell-list">
-            {filteredSpells.length ? (
-              filteredSpells.map(s => <Card key={s.name} spell={s} />)
-            ) : (
-              <div className="Description-text">Alas, such a spell has yet to be crafted.</div>
-            )}
+          <div className='Spell-list'>
+            {filteredSpells.length ? filteredSpells.map(s => <Card key={s.name} spell={s} />) : <div className='Description-text'>Alas, such a spell has yet to be crafted.</div>}
           </div>
         </div>
       )}
