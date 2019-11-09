720185b1939cd387ce8c2563eaa6397168982bf9
mdesson <michael.desson@gmail.com>
2019-11-08 22:46:20 -0500

    Made basis for Account view

--- src/views/Account.js
+++ src/views/Account.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { LOGIN, LOGOUT } from '../redux/actionTypes'
+import './Account.css'
 
 // TODO: For data access, https://firebase.google.com/docs/firestore/security/overview
 // Better link: https://firebase.google.com/docs/firestore/security/rules-conditions
@@ -11,28 +12,62 @@ import { LOGIN, LOGOUT } from '../redux/actionTypes'
 //   .set({ name: 'mike' })
 
 const Account = ({ auth, provider }) => {
+  const [loggingIn, setLoggingIn] = useState(false)
   const dispatch = useDispatch()
   const user = useSelector(state => state.user)
 
   const signIn = async () => {
+    setLoggingIn(true)
+
     await auth.signInWithPopup(provider).catch(function(error) {
       console.error('ERROR ON SIGNIN:' + error)
     })
-    const user = auth.currentUser
-    dispatch({ type: LOGIN, payload: { name: user.displayName, uid: user.uid } })
+
+    const loggedinUser = auth.currentUser
+    dispatch({ type: LOGIN, payload: { name: loggedinUser.displayName, uid: loggedinUser.uid } })
+
+    setLoggingIn(false)
   }
 
-  var signOut = async () => {
+  const signOut = async () => {
     await auth.signOut().catch(function(error) {
       console.error('ERROR ON SIGNOUT:' + error)
     })
     dispatch({ type: LOGOUT })
   }
+
   return (
-    <div>
-      <div onClick={signIn}>Login</div>
-      <div onClick={signOut}>Logout</div>
-      <div>Hi, {user ? user.name : 'stranger'}.</div>
+    <div className='Account-container'>
+      <div className='Account-greeting'>
+        {loggingIn && <div>Logging In...</div>}
+        {user && !loggingIn && (
+          <div>
+            Welcome, {user.name}{' '}
+            <span className='Account-sign-in-out' onClick={signOut}>
+              (Sign Out)
+            </span>
+          </div>
+        )}
+        {!user && !loggingIn && (
+          <div>
+            Welcome, stranger.{' '}
+            <span className='Account-sign-in-out' onClick={signIn}>
+              Sign In or Create Account
+            </span>{' '}
+            to access this page's features.
+          </div>
+        )}
+      </div>
+      <div>Use this page to manage your characters. You can:</div>
+      <div>
+        <ul>
+          <li>Create characters</li>
+          <li>Change their class(es)</li>
+          <li>Set their level</li>
+          <li>Delete characters</li>
+        </ul>
+      </div>
+      <div></div>
     </div>
   )
 }
