f0899e3ae220e9c4ac87deb4f2baac6b16ba6eec
mdesson <michael.desson@gmail.com>
2019-11-09 12:35:47 -0500

    Added modal empty modal to Character Manager

--- src/components/CharacterManager.js
+++ src/components/CharacterManager.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import './CharacterManager.css'
 
+// TODO: Sanitize user input
 const CharacterRows = ({ characterArray }) => {
-  return characterArray.map(character => (
+  return characterArray.map(character => <CharacterRow key={character.name} character={character} />)
+}
+
+const CharacterRow = ({ character }) => {
+  return (
     <tr key={character.name}>
       <td>{character.name}</td>
       <td>{character.class}</td>
       <td>{character.level}</td>
       <td className='Account-button'>Modify</td>
     </tr>
-  ))
+  )
+}
+
+const Modal = ({ hideModal }) => {
+  return (
+    <div className='CharacterManager-modal'>
+      <div className='CharacterManager-modal-content'>
+        <span className='CharacterManager-modal-close' onClick={hideModal}>
+          &times;
+        </span>
+        <p>Some text in the Modal..</p>
+      </div>
+    </div>
+  )
 }
 
 const AddCharacter = () => {
@@ -26,6 +44,7 @@ const EditCharacter = () => {
 }
 
 const CharacterManager = () => {
+  const [showModal, setShowModal] = useState(false)
   const characters = useSelector(state => state.characters)
 
   return (
@@ -42,7 +61,10 @@ const CharacterManager = () => {
         </tbody>
       </table>
       {!characters.length && <div className='CharacterManager-message'>You haven't created any characters. Use the button below to get started!</div>}
-      <div className='Account-button'>Add New</div>
+      <div className='Account-button' onClick={() => setShowModal(true)}>
+        Add New
+      </div>
+      {showModal && <Modal hideModal={() => setShowModal(false)} />}
     </div>
   )
 }
