aad54efb3f256f005fdc6de14cb5be56a91256da
mdesson <michael.desson@gmail.com>
2019-11-08 20:32:47 -0500

    Deleted selectors.js

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,4 +1,3 @@
 export const LOAD_ALL_SPELLS = 'LOAD_ALL_SPELLS'
 export const SET_SPELL_FILTER = 'SET_SPELL_FILTER'
-export const SET_FILTERED_SPELLS = 'SET_FILTERED_SPELLS'
 export const SET_USER = 'SET_USER'
--- src/redux/selectors.js
+++
@@ -1,2 +0,0 @@
-export const getAllSpells = store => store.allspells
-export const getFilter = store => store.spellfilter
