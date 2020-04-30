8fadcfc4d1400e534e650498769b3e44a454a278
mdesson <michael.desson@gmail.com>
2020-04-29 22:22:34 -0400

    Added help text when user has no characters on spellbook page

--- src/views/SpellBook.js
+++ src/views/SpellBook.js
@@ -16,6 +16,7 @@ import './SpellBook.css'
 const SpellBook = () => {
   const spells = useSelector(state => state.allspells)
   const user = useSelector(state => state.user)
+  const characters = useSelector(state => state.characters)
   const dispatch = useDispatch()
 
   // If spells have not yet been loaded globally
@@ -43,34 +44,37 @@ const SpellBook = () => {
     <div className="SpellBook-container">
       {user && <CharacterSelect key="select" />}
       <p className="Description-text">
-        {user
-          ? "Click the book icon to manage your spells. A closed book means you know the spell, and an open book means you've prepared the spell."
-          : 'Create an account in order to create characters and manage their known and prepared spells.'}
+        {!user
+          ? 'Create an account in order to create characters and manage their known and prepared spells.'
+          : !characters || characters.length === 0
+          ? 'Create a character to get started with managing your spells.'
+          : "Click the book icon to manage your spells. A closed book means you know the spell, and an open book means you've prepared the spell."}
       </p>
-      {user && [
-        <SpellGroup
-          title="Prepared Spells"
-          spellStore="preparedspells"
-          filteredSpellStore="filteredpreparedspells"
-          filterName="preparedspellsfilter"
-          setFilterFunc={setPreparedSpellsFilter}
-          setSpellsFunc={setFilteredPreparedSpells}
-          isSearchable={true}
-          noSpellsFoundString="You have no spells prepared for the day."
-          key="prepared"
-        />,
-        <SpellGroup
-          title="Known Spells"
-          spellStore="knownspells"
-          filteredSpellStore="filteredknownspells"
-          filterName="knownspellsfilter"
-          setFilterFunc={setKnownSpellsFilter}
-          setSpellsFunc={setFilteredKnownSpells}
-          isSearchable={true}
-          noSpellsFoundString="You know no unprepared spells."
-          key="known"
-        />
-      ]}
+      {characters &&
+        characters.length !== 0 && [
+          <SpellGroup
+            title="Prepared Spells"
+            spellStore="preparedspells"
+            filteredSpellStore="filteredpreparedspells"
+            filterName="preparedspellsfilter"
+            setFilterFunc={setPreparedSpellsFilter}
+            setSpellsFunc={setFilteredPreparedSpells}
+            isSearchable={true}
+            noSpellsFoundString="You have no spells prepared for the day."
+            key="prepared"
+          />,
+          <SpellGroup
+            title="Known Spells"
+            spellStore="knownspells"
+            filteredSpellStore="filteredknownspells"
+            filterName="knownspellsfilter"
+            setFilterFunc={setKnownSpellsFilter}
+            setSpellsFunc={setFilteredKnownSpells}
+            isSearchable={true}
+            noSpellsFoundString="You know no unprepared spells."
+            key="known"
+          />
+        ]}
     </div>
   )
 }
